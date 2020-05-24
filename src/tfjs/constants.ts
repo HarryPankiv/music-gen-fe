eab01bfb8f5f3c56e774eb61ba49b07c4b821bb3
Ihor Pankiv <harry.pankiv@gmail.com>
2020-05-24 17:42:29 +0300

    feat: added tfjs constants

---
+++ src/tfjs/constants.ts
@@ -0,0 +1,8 @@
+export const MAX_PAN = 0.2;
+export const STEPS_PER_QUARTER = 24;
+export const Z_DIM = 256;
+export const QPM = 120;
+export const HUMANIZE_SECONDS = 0.01;
+export const MIN_DRUM = 35;
+export const MAX_DRUM = 81;
+
