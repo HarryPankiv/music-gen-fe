386db9176efc6d702bd3828a80ddcc88583af5f7
Ihor Pankiv <harry.pankiv@gmail.com>
2020-05-24 17:45:01 +0300

    feat: added GenerateAudioPage

---
+++ src/pages/GenerateAudioPage/index.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router";
+import { generateMusic } from "../../tfjs/generateMusic";
+import { modes, scalesByMode, keys } from "../../audio/constants";
+import { RadioButtonGroup, RangeInput, Box, Button } from "grommet";
+
+export const GenerateAudioPage = () => {
+  const { push } = useHistory();
+  const [tonic, setTonic] = useState("");
+  const [mode, setMode] = useState("");
+  const [scale, setScale] = useState("");
+  const [tempo, setTempo] = useState("");
+
+  const generate = () => {
+    const scale = "F";
+
+    generateMusic(scale);
+    push("/generated/:id");
+  };
+
+  return (
+    <Box
+      margin="10% 15%"
+      // background="rgba(61, 19, 141, 0.1);"
+      pad="40px"
+    >
+      <RadioButtonGroup
+        name="keys"
+        options={keys}
+        onChange={(e) => setTonic(e.target.value)}
+        value={tonic}
+      />
+      <RadioButtonGroup
+        name="modes"
+        options={modes}
+        onChange={(e) => setMode(e.target.value)}
+        value={mode}
+      />
+      {mode && (
+        <RadioButtonGroup
+          name="scale"
+          options={scalesByMode[mode]}
+          onChange={(e) => setScale(e.target.value)}
+          value={scale}
+        />
+      )}
+      <RangeInput
+        name="tempo"
+        min={30}
+        max={180}
+        value={tempo}
+        onChange={(e) => setTempo(e.target.value)}
+      />
+      <Button onClick={generate}>generate music</Button>
+    </Box>
+  );
+};
