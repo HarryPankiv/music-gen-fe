706549a795423aee5a9861bef24480d7dee6bf6a
Ihor Pankiv <harry.pankiv@gmail.com>
2020-05-24 17:43:19 +0300

    feat: added export audio button

---
+++ src/pages/GeneratedAudioPage/Buttons/ExportAudioButton.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Button } from "grommet";
+import { Music } from "grommet-icons";
+
+export const ExportAudioButton = () => {
+  const exportAudio = async () => {};
+  return (
+    <Button
+      onClick={exportAudio}
+      label="Export .mp3"
+      icon={<Music color="brand" size="medium" />}
+      margin="small"
+      reverse
+    />
+  );
+};
