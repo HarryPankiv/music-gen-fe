9b050efd88dcca98ee5bf1fb5e01707c2e69d96a
Ihor Pankiv <harry.pankiv@gmail.com>
2020-05-24 17:39:22 +0300

    feat: added grommet theme config

---
+++ src/theme/theme.ts
@@ -0,0 +1,20 @@
+import { ThemeType } from "grommet";
+
+export const theme: ThemeType = {
+    global: {
+        font: {
+            family: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"'
+        },
+        colors: {
+            text: "brand"
+        }
+    },
+    button: {
+        
+    },
+    icon: {
+        size: {
+            medium: "18px"
+        }
+    }
+}
