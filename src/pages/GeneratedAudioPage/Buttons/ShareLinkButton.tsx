91bbc3ce7814df5ac2de3c0508ce934997d653cc
Ihor Pankiv <harry.pankiv@gmail.com>
2020-06-01 14:18:38 +0300

    feat: added copied status for share button

--- src/pages/GeneratedAudioPage/Buttons/ShareLinkButton.tsx
+++ src/pages/GeneratedAudioPage/Buttons/ShareLinkButton.tsx
@@ -1,24 +1,34 @@
-import React from "react";
-import { Button } from "grommet";
+import React, { useState } from "react";
+import { Button, Box } from "grommet";
 import copy from "copy-to-clipboard";
 import { useRouteMatch } from "react-router";
 
 import { ShareAlternative } from "@styled-icons/entypo/ShareAlternative";
 
+import "./style.scss";
+
 export const ShareLinkButton = () => {
+  const [copied, setCopied] = useState(false);
   const { url } = useRouteMatch();
 
   const shareLink = async () => {
     copy(`${window.location.host}${url}`);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 5000);
   };
 
   return (
-    <Button
-      onClick={shareLink}
-      label="Share"
-      icon={<ShareAlternative size="18" />}
-      margin="small"
-      reverse
-    />
+    <Box height="44px">
+      <Button
+        onClick={shareLink}
+        label={copied ? "Copied url" : "Share"}
+        icon={copied ? undefined : <ShareAlternative size="18" />}
+        id={copied ? "copied-btn" : undefined}
+        margin="small"
+        reverse
+      />
+    </Box>
   );
 };
