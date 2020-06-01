c09b38f7c463714f657bb2a5cbc600ebb76b46bd
Ihor Pankiv <harry.pankiv@gmail.com>
2020-06-01 14:15:12 +0300

    feat: added page component

---
+++ src/components/Page/Page.tsx
@@ -0,0 +1,13 @@
+import React, { useContext } from "react";
+import { ResponsiveContext, Box } from "grommet";
+
+export const Page = ({ children }) => {
+  const size = useContext(ResponsiveContext);
+  const width = {
+    small: "400px",
+    medium: "700px",
+    large: "900px",
+  };
+
+  return <Box margin={{top: "80px", bottom: "100px"}} alignSelf="center" width={width[size]}>{children}</Box>;
+};
