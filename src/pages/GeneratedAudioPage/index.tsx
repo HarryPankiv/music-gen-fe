1bb48bf97454d6368ac38bdf90cb49c06c945635
Ihor Pankiv <harry.pankiv@gmail.com>
2020-06-01 14:16:54 +0300

    feat: updated request url

--- src/pages/GeneratedAudioPage/index.tsx
+++ src/pages/GeneratedAudioPage/index.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { useHistory } from "react-router";
+import { useHistory, useRouteMatch } from "react-router";
 import { useRequest } from "@umijs/hooks";
 import { Heading, Box, Button } from "grommet";
 
-import { ExportAudioButton } from "./Buttons/ExportAudioButton";
 import { ExportMidiButton } from "./Buttons/ExportMidiButton";
 import { ShareLinkButton } from "./Buttons/ShareLinkButton";
 
@@ -12,9 +11,11 @@ import { getBaseURL } from "../../api/getBaseURL";
 import { ScreenLoader } from "../../components/ScreenLoader";
 
 export const GeneratedAudio = () => {
-  const { push } = useHistory() 
+  const { push } = useHistory()
+  const { params:  { id } } = useRouteMatch<{ id: string}>()
+
   const { data, loading } = useRequest({
-    url: "/:id",
+    url: `/getGeneratedAudio?projectId=${id}`,
     type: "get",
     prefix: getBaseURL(),
   });
@@ -23,26 +24,23 @@ export const GeneratedAudio = () => {
     return <ScreenLoader />;
   }
 
-  // if (!data) {
-  //   return <div>
-  //     No audio for this url
-  //   </div>
-  // }
+  if (!data) {
+    return <div>
+      No audio for this url
+    </div>
+  }
 
   return (
     <Box
       margin="10% 15%"
-      align="center"
       pad="40px"
     >
-      <Heading>Hey, here is your sequence</Heading>
-      <Box margin={{bottom: "60px"}}>
+      <Heading textAlign="center">Hey, here is your sequence</Heading>
+      <Box margin={{bottom: "60px"}} width="312px" alignSelf="center">
         <Button primary label="Generate new sequence" size="medium" onClick={() => push('/')} />
       </Box>
-      {/* <Player sequences={data.instruments} /> */}
-      <Player sequences={data?.sequences} />
-      <Box justify="between" direction="row" margin={{top: "60px"}}>
-        <ExportAudioButton />
+      <Player sequences={data?.sequence} />
+      <Box justify="between" direction="row" margin={{top: "60px"}} height="84px">
         <ExportMidiButton />
         <ShareLinkButton />
       </Box>
