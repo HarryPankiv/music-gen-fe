6e84adca42e1fbc9fa35b31344bbe794a8a86c90
Ihor Pankiv <harry.pankiv@gmail.com>
2020-06-01 14:02:29 +0300

    feat: added config for api url to depend on env

--- src/api/getBaseURL.ts
+++ src/api/getBaseURL.ts
@@ -1,1 +1,6 @@
-export const getBaseURL = () => 'https://bfybqjkxj4.execute-api.us-east-1.amazonaws.com/dev'
+const apiUrlByEnv = {
+    local: 'http://localhost:7000/dev',
+    staging: 'https://bfybqjkxj4.execute-api.us-east-1.amazonaws.com/dev'
+}
+
+export const getBaseURL = () => apiUrlByEnv[process.env.REACT_APP_ENV!] || apiUrlByEnv.staging 
